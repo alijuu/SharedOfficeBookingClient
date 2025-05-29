f11e54d067fd156be920d489135bdb6747e61d31
Ahmed210901 <ahmedpilav22@gmail.com>
2025-05-29 12:10:07 +0200

    Added svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-
+import svgr from "vite-plugin-svgr"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,7 +10,7 @@ export default defineConfig({
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     tailwindcss(),
     react(),
-    
+    svgr(),
     // ...,
   ],
   server: {
