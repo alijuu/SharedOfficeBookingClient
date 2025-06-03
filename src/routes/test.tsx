3a472523a58fc9dce72b3b782ce4326795d7e939
alijuu <67058944+alijuu@users.noreply.github.com>
2025-06-03 22:04:53 +0200

    Update test.tsx

--- src/routes/test.tsx
+++ src/routes/test.tsx
@@ -1,6 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import {BaseLayout} from '../components/layout/BaseLayout.tsx';
-import DeskDetails from "../components/DeskDetails/DeskDetails.tsx";
 
 
 export const Route = createFileRoute('/test')({
@@ -8,7 +6,5 @@ export const Route = createFileRoute('/test')({
 })
 
 function RouteComponent() {
-  return <BaseLayout>
-    <DeskDetails/>
-  </BaseLayout>
+  return <>O</>
 }
